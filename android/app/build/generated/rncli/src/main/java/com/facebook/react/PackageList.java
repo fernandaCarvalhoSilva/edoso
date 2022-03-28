78584ba9053ac94ccf049238f4330b6424e50a1d
fernandaCarvalhoSilva <fer.carvalho153@gmail.com>
2022-03-28 19:53:29 -0300

    add android an ios

---
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -0,0 +1,109 @@
+
+package com.facebook.react;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Resources;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainPackageConfig;
+import com.facebook.react.shell.MainReactPackage;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+// @notifee/react-native
+import io.invertase.notifee.NotifeePackage;
+// @react-native-async-storage/async-storage
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+// @react-native-community/blur
+import com.cmcewen.blurview.BlurViewPackage;
+// @react-native-community/cameraroll
+import com.reactnativecommunity.cameraroll.CameraRollPackage;
+// @react-native-voice/voice
+import com.wenkesj.voice.VoicePackage;
+// react-native-camera
+import org.reactnative.camera.RNCameraPackage;
+// react-native-contacts
+import com.rt2zz.reactnativecontacts.ReactNativeContacts;
+// react-native-date-picker
+import com.henninghall.date_picker.DatePickerPackage;
+// react-native-gesture-handler
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+// react-native-image-picker
+import com.imagepicker.ImagePickerPackage;
+// react-native-installed-application
+import com.installedapplication.RNInstalledApplicationPackage;
+// react-native-push-notification
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+// react-native-safe-area-context
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
+// react-native-send-intent
+import com.burnweb.rnsendintent.RNSendIntentPackage;
+// react-native-svg
+import com.horcrux.svg.SvgPackage;
+// react-native-vector-icons
+import com.oblador.vectoricons.VectorIconsPackage;
+
+public class PackageList {
+  private Application application;
+  private ReactNativeHost reactNativeHost;
+  private MainPackageConfig mConfig;
+
+  public PackageList(ReactNativeHost reactNativeHost) {
+    this(reactNativeHost, null);
+  }
+
+  public PackageList(Application application) {
+    this(application, null);
+  }
+
+  public PackageList(ReactNativeHost reactNativeHost, MainPackageConfig config) {
+    this.reactNativeHost = reactNativeHost;
+    mConfig = config;
+  }
+
+  public PackageList(Application application, MainPackageConfig config) {
+    this.reactNativeHost = null;
+    this.application = application;
+    mConfig = config;
+  }
+
+  private ReactNativeHost getReactNativeHost() {
+    return this.reactNativeHost;
+  }
+
+  private Resources getResources() {
+    return this.getApplication().getResources();
+  }
+
+  private Application getApplication() {
+    if (this.reactNativeHost == null) return this.application;
+    return this.reactNativeHost.getApplication();
+  }
+
+  private Context getApplicationContext() {
+    return this.getApplication().getApplicationContext();
+  }
+
+  public ArrayList<ReactPackage> getPackages() {
+    return new ArrayList<>(Arrays.<ReactPackage>asList(
+      new MainReactPackage(mConfig),
+      new NotifeePackage(),
+      new AsyncStoragePackage(),
+      new BlurViewPackage(),
+      new CameraRollPackage(),
+      new VoicePackage(),
+      new RNCameraPackage(),
+      new ReactNativeContacts(),
+      new DatePickerPackage(),
+      new RNGestureHandlerPackage(),
+      new ImagePickerPackage(),
+      new RNInstalledApplicationPackage(),
+      new ReactNativePushNotificationPackage(),
+      new SafeAreaContextPackage(),
+      new RNSendIntentPackage(),
+      new SvgPackage(),
+      new VectorIconsPackage()
+    ));
+  }
+}
